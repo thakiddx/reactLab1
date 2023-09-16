364b31b410c57e5104ee2245f6f99dc3a8d34d5c
xmalone0920 <xmalone0920@gmail.com>
2023-09-16 16:31:31 -0700

    buttonColor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
 import Header from "./components/Header/header.jsx";
+import Button from './components/Button/button.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Button />
     </div>
   );
 }
